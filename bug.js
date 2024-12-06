7d71b8ea2a3a71c0bb97844991bb9fef308f68f7
BugHunter-X <askzenai@gmail.com>
2024-12-06 07:52:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Unexpected error handling
+  if (req.url === '/error') {
+    throw new Error('Something went wrong!');
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
